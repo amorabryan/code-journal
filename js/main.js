9161c7e9d7f6716e756a066bd9e4fefd3ea478fe
Bryan Amora <amorabryan@yahoo.com>
2023-05-29 22:39:20 +0000

    Added a submit event listener - Issue #1

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ $photoURL.addEventListener('input', function (event) {
     $url.src = event.target.value;
   }
 });
+
+const $codeJournal = document.querySelector('#codeJournal');
+const journalElements = $codeJournal.elements;
+
+$codeJournal.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const journalData = {};
+
+  const title = journalElements.title.name;
+  const photoURL = journalElements.photoURL.name;
+  const notes = journalElements.notes.name;
+
+  const titleValue = journalElements.title.value;
+  const photoURLValue = journalElements.photoURL.value;
+  const notesValue = journalElements.notes.value;
+
+  journalData.entryId = data.nextEntryId;
+
+  data.nextEntryId++;
+
+  journalData[title] = titleValue;
+  journalData[photoURL] = photoURLValue;
+  journalData[notes] = notesValue;
+
+  const entriesArray = data.entries;
+  entriesArray.push(journalData);
+
+  $url.src = 'images/placeholder-image-square.jpg';
+
+  $codeJournal.reset();
+});
