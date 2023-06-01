a5a64ffe6acc6ed598635a4ee9a342a291c17937
Bryan Amora <amorabryan@yahoo.com>
2023-06-01 22:16:37 +0000

    Updated the submit callback, updated the DOMContentLoaded callback, added anchors - Issue #2

--- js/main.js
+++ js/main.js
@@ -38,6 +38,14 @@ $codeJournal.addEventListener('submit', function (event) {
 
   $url.src = 'images/placeholder-image-square.jpg';
 
+  $journalList.prepend(renderEntry(journalData));
+
+  viewSwap('entries');
+
+  if (data.entries === []) {
+    toggleNoEntries();
+  }
+
   $codeJournal.reset();
 });
 
@@ -60,23 +68,23 @@ function renderEntry(entry) {
   const $firstEntry = document.createElement('li');
   $firstEntry.setAttribute('class', 'row');
 
-  const $firstCol = document.createElement('div');
-  $firstCol.setAttribute('class', 'column-full column-half');
-  $firstEntry.appendChild($firstCol);
+  const $firstDiv = document.createElement('div');
+  $firstDiv.setAttribute('class', 'column-full column-half');
+  $firstEntry.appendChild($firstDiv);
 
   const $divImg = document.createElement('div');
   $divImg.setAttribute('class', 'img');
-  $firstCol.appendChild($divImg);
+  $firstDiv.appendChild($divImg);
+
+  const $secondCol = document.createElement('div');
+  $secondCol.setAttribute('class', 'column-full column-half');
+  $firstEntry.appendChild($secondCol);
 
   const $img = document.createElement('img');
   $img.setAttribute('src', entry.userPhoto);
   $img.setAttribute('alt', entry.userTitle);
   $img.setAttribute('class', 'photo');
   $divImg.appendChild($img);
-
-  const $secondCol = document.createElement('div');
-  $secondCol.setAttribute('class', 'column-full column-half');
-  $firstCol.append($secondCol);
 
   const $h2 = document.createElement('h2');
   $h2.setAttribute('class', 'title');
@@ -94,9 +102,47 @@ function renderEntry(entry) {
 const $journalList = document.querySelector('.journal-list');
 
 document.addEventListener('DOMContentLoaded', function () {
-  for (let i = 0; i < data.entries.length; i++) {
+  const lastView = data.view;
+  if (lastView) {
+    viewSwap(lastView);
+  } for (let i = 0; i < data.entries.length; i++) {
     const $newEntry = renderEntry(data.entries[i]);
     $journalList.appendChild($newEntry);
+  } if (data.entries === []) {
+    toggleNoEntries();
   }
 }
 );
+
+const $noEntries = document.querySelector('.no-entries');
+
+function toggleNoEntries() {
+  if (data.entries === []) {
+    $noEntries.className = 'no-entries';
+  } else {
+    $noEntries.className = 'no-entries' + ' ' + 'hidden';
+  }
+}
+
+const $view = document.querySelectorAll('.view');
+
+function viewSwap(viewSwitch) {
+  data.view = viewSwitch;
+  for (let i = 0; i < $view.length; i++) {
+    if (viewSwitch === $view[i].getAttribute('data-view')) {
+      $view[i].classList.remove('hidden');
+    } else {
+      $view[i].classList.add('hidden');
+    }
+  }
+}
+
+const $entryLaunch = document.querySelector('.entry-launch');
+$entryLaunch.addEventListener('click', function (event) {
+  viewSwap('entries');
+});
+
+const $formLaunch = document.querySelector('.form-launch');
+$formLaunch.addEventListener('click', function (event) {
+  viewSwap('entry-form');
+});
